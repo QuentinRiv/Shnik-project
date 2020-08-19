1d82fbc1819139a3917e820bab84edcde4b4ff54
QuentinRiv <32091703+QuentinRiv@users.noreply.github.com>
2020-08-19 21:39:08 +0200

    'error_handler'

--- app.py
+++ app.py
@@ -46,18 +46,27 @@ class Variante(db.Model):
         return '<Var %r>' % self.id
 
 
+path = "./words_albanian.txt"
+path_transl = "./transl_alb.txt"
 
-path = "C:\\Users\\quent\\Desktop\\Projet\\words_albanian.txt"
-path_transl = "C:\\Users\\quent\\Desktop\\Projet\\transl_alb.txt"
+
 def fillDB(path):
-    file1 = open(path, 'r', encoding='utf-8')
+    try:
+        file1 = open(path, 'r', encoding='utf-8')
+    except:
+        return make_response(jsonify({"message": "Problem to open file " + path}), 404)
+
     lines = file1.readlines()
     words = []
     for line in lines[2:]:
         if line != '\n':
             words += [line[:-1]]
 
-    file2 = open(path_transl, 'r', encoding='utf-8')
+    try:
+        file2 = open(path_transl, 'r', encoding='utf-8')
+    except:
+        return make_response(jsonify({"message": "Problem to open file " + path_transl}), 404)
+
     lines_transl = file2.readlines()
     words2 = []
     for line in lines_transl[2:]:
@@ -66,19 +75,19 @@ def fillDB(path):
 
     print(words)
     for i, word in enumerate(words):
-        newIm = Image(name=word, image_path='/static/data/leksik/' + word + '.jpg')
+        newIm = Image(
+            name=word, image_path='/static/data/leksik/' + word + '.jpg')
         newVar = Variante(name=words2[i], lName=newIm)
         db.session.add(newIm)
         db.session.add(newVar)
         try:
             db.session.commit()
         except:
-            print("***Problème***")
             return "Problème pour le commit"
     print("DB correctly done")
     file1.close()
 
-    return jsonify(dico), 200
+    return 0
 
 
 def checkip():
@@ -98,24 +107,33 @@ def checkip():
 
     return 'OK'
 
+
 @app.route('/')
 def home():
     if checkip() != 'OK':
         return make_response(jsonify({"message": checkip()}), 200)
-    # fillDB(path)
+    ans = fillDB(path)
+    if ans != 0:
+        return ans
 
     return render_template('welcome.html')
 
 
-
-
 @app.route('/image/<string:name>', methods=['POST', 'GET'])
 def index(name):
-    image_query = Image.query.filter_by(name=name).first()                  # Get the corresponding image
-    vars = image_query.info                                                 # Get the variantes of the images
-    words = np.array([vari.name for vari in vars])                          # Put the words into an array (np is used to sort)
-    scores = np.array([vari.count for vari in vars])                        # Get the scores
-    path_im = image_query.image_path                                        # Path of the image
+    # Get the corresponding image
+    image_query = Image.query.filter_by(name=name).first()
+
+    if image_query is None:
+        return 'No image found with the name ' + name
+    # Get the variantes of the images
+    vars = image_query.info
+    # Put the words into an array (np is used to sort)
+    words = np.array([vari.name for vari in vars])
+    # Get the scores
+    scores = np.array([vari.count for vari in vars])
+    # Path of the image
+    path_im = image_query.image_path
 
     # Order the words, according to their score
     ordre = scores.argsort()[::-1]
@@ -143,6 +161,10 @@ def create_entry():
     name = req['name']
     new_words = req['newwords'].lower()
     image_query = Image.query.filter_by(name=name).first()
+
+    if image_query is None:
+        return 'No image found with the name ' + name
+
     image_query.nb_ans += 1
 
     # Add new words
@@ -152,31 +174,33 @@ def create_entry():
             db.session.add(newVar)
             db.session.commit()
         except:
-            return "problème pour le commit"
+            return "problem for the commit"
 
     # +1 for selected words
     selected = req['selwords']
     if (selected != []):
         for selection in selected:
             selElem = Variante.query.filter_by(name=selection).first()
+            if selElem is None:
+                return 'No Variance found with the name ' + flag_word
             selElem.count += 1
             try:
                 db.session.commit()
-                print("MAJ ok")
             except:
-                return "Problème pour le commit"
+                return "problem for the commit"
 
     # Flag the words
     flagge = req['flagwords']
     if (flagge != []):
         for flag_word in flagge:
             selElem = Variante.query.filter_by(name=flag_word).first()
+            if selElem is None:
+                return 'No Variance found with the name ' + flag_word
             selElem.flag += 1
             try:
                 db.session.commit()
-                print("MAJ ok")
             except:
-                return "Problème pour le commit"
+                return "problem for the commit"
 
     return make_response(jsonify({"message": "OK : word(s) added"}), 200)
 
@@ -191,6 +215,9 @@ def delete_entry():
     # Get the info for adding the new word(s)
     name = req['name']
     image_query = Image.query.filter_by(name=name).first()
+
+    if image_query is None:
+        return 'No image found with the name ' + name
 
     for w2del in req['selwords']:
         elem2del = Variante.query.filter_by(name=w2del).first()
@@ -209,6 +236,7 @@ def delete_entry():
 def stats():
     return render_template('stats.html')
 
+
 @app.route('/alldata')
 def data():
     # Get the corresponding image
@@ -220,9 +248,9 @@ def data():
         names = np.array([var.name for var in vars])
         scores = np.array([var.count for var in vars])
         flags = np.array([var.flag for var in vars])
-        order = scores.argsort()
+        order = scores.argsort()[::-1]
         names_ordered = names[order].tolist()
-        scores_ordered = np.sort(scores).tolist()
+        scores_ordered = np.sort(scores).tolist()[::-1]
         flags_ordered = flags[order].tolist()
         nb_ans = image.nb_ans
         dico[image.name] = {'variance': names_ordered,
@@ -231,9 +259,7 @@ def data():
                             'nb_ans': nb_ans}
         dico['names'] += [image.name]
 
-
     return jsonify(dico)
-
 
 
 if __name__ == "__main__":
